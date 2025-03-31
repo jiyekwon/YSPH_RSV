d9eb27955211c618e13a0eb0ab5b3acc2e4842b4
Seth Redmond <snr28@login1.mccleary.ycrc.yale.edu>
2025-03-31 16:34:37 -0400

    adding transnet parameter search data

---
+++ analysis/250106_transnet_reconstruction/run_all_transnets.sh
@@ -0,0 +1,20 @@
+#for S in 3 5 7 9; do
+for S in 5; do
+    for M in 1e-5 1e-6; do
+    	Rscript run_transnet_juniper.R -i input_data         -m ${M} -M -s ${S} -o rsv_m${M}fix_s${S}_incvcfs
+    	Rscript run_transnet_juniper.R -i input_data         -m ${M}    -s ${S} -o rsv_m${M}var_s${S}_incvcfs
+    	Rscript run_transnet_juniper.R -i input_data_noivars -m ${M} -M -s ${S} -o rsv_m${M}fix_s${S}_novcfs
+    	Rscript run_transnet_juniper.R -i input_data_noivars -m ${M}    -s ${S} -o rsv_m${M}var_s${S}_novcfs
+    done
+done
+
+
+for S in 7 9; do
+    for M in 1e-4 1e-5 1e-6; do
+    	Rscript run_transnet_juniper.R -i input_data         -m ${M} -M -s ${S} -o rsv_m${M}fix_s${S}_incvcfs
+    	Rscript run_transnet_juniper.R -i input_data         -m ${M}    -s ${S} -o rsv_m${M}var_s${S}_incvcfs
+    	Rscript run_transnet_juniper.R -i input_data_noivars -m ${M} -M -s ${S} -o rsv_m${M}fix_s${S}_novcfs
+    	Rscript run_transnet_juniper.R -i input_data_noivars -m ${M}    -s ${S} -o rsv_m${M}var_s${S}_novcfs
+    done
+done
+
