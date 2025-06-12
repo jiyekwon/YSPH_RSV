67584f4b0ed7a50a33116d6c9db2f8b4211e8e72
Seth Redmond <snr28@login1.mccleary.ycrc.yale.edu>
2025-06-12 17:31:50 -0400

    parameter search net reconstruction

--- analysis/250106_transnet_reconstruction/run_all_transnets.sh
+++ analysis/250106_transnet_reconstruction/run_all_transnets.sh
@@ -1,21 +1,8 @@
 #for S in 3 5 7 9; do
-for S in 7; do
-    for M in 1e-5; do
-	Rscript run_transnet_juniper.R -i input_data_noivars -m ${M} -M -s ${S} -o rsv_m${M}fix_s${S}_novcfs
-	Rscript run_transnet_juniper.R -i input_data_noivars -m ${M}    -s ${S} -o rsv_m${M}var_s${S}_novcfs
-    done
-    for M in 1e-6; do
-    	Rscript run_transnet_juniper.R -i input_data         -m ${M} -M -s ${S} -o rsv_m${M}fix_s${S}_incvcfs
-    	Rscript run_transnet_juniper.R -i input_data         -m ${M}    -s ${S} -o rsv_m${M}var_s${S}_incvcfs
-    	Rscript run_transnet_juniper.R -i input_data_noivars -m ${M} -M -s ${S} -o rsv_m${M}fix_s${S}_novcfs
-    	Rscript run_transnet_juniper.R -i input_data_noivars -m ${M}    -s ${S} -o rsv_m${M}var_s${S}_novcfs
-    done
-done
-
-
 for S in 9; do
-    for M in 1e-4 1e-5 1e-6; do
-    	Rscript run_transnet_juniper.R -i input_data         -m ${M} -M -s ${S} -o rsv_m${M}fix_s${S}_incvcfs
+#    for M in 1e-4 1e-5 1e-6; do
+    for M in 1e-6; do
+#    	Rscript run_transnet_juniper.R -i input_data         -m ${M} -M -s ${S} -o rsv_m${M}fix_s${S}_incvcfs
     	Rscript run_transnet_juniper.R -i input_data         -m ${M}    -s ${S} -o rsv_m${M}var_s${S}_incvcfs
     	Rscript run_transnet_juniper.R -i input_data_noivars -m ${M} -M -s ${S} -o rsv_m${M}fix_s${S}_novcfs
     	Rscript run_transnet_juniper.R -i input_data_noivars -m ${M}    -s ${S} -o rsv_m${M}var_s${S}_novcfs
