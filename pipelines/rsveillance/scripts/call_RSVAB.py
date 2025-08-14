5977ce980415253444966c983a28462d57248118
Seth Redmond <snr28@login1.mccleary.ycrc.yale.edu>
2025-08-14 07:16:25 -0400

    nextstrain pipeline running

--- pipelines/rsveillance/scripts/call_RSVAB.py
+++ pipelines/rsveillance/scripts/call_RSVAB.py
@@ -104,8 +104,8 @@ else:
 combinations = pd.merge(combinations,alignstats[alignstatsnames],on=['sample','mashcall'])
 
 #fill across call / mashcall columns
-calls['mashcall'] = calls['mashcall'].fillna(calls['call'])
-calls['call'] = calls['call'].fillna(calls['mashcall'])
+#calls['mashcall'] = calls['mashcall'].fillna(calls['call'])
+#calls['call'] = calls['call'].fillna(calls['mashcall'])
 
 calls.to_csv("{}_calls.txt".format(outfile),sep="\t",index=False)
 combinations.to_csv("{}_alignstats.txt".format(outfile),sep="\t",index=False,header=False)
